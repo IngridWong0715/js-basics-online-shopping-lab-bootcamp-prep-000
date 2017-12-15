b6d46d7146feda5c7a0396a3917237d4840a072e
Ingrid Wong <yw340@georgetown.edu>
2017-12-15 19:28:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber !== undefined){
     console.log(`Your total cost is $${total()},
-    which will be charged to the card ${cardNumber}`);
+    which will be charged to the card ${cardNumber}.`);
     cart = [];
   } else {
     console.log("Sorry, we don't have a credit card on file for you.");
