77fbe937e41e46a99cc19eb8c888649034bc0a17
Ingrid Wong <yw340@georgetown.edu>
2017-12-15 19:07:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function removeFromCart(item) {
     if (cart[i].hasOwnProperty(item)){
       cart = cart.splice(i,1);
       return cart;
-    }
+    } else return "That item is not in your cart."
   }
 
 }
