a3694e96f26ea6cb5117ac3028230df241288cd0
Ingrid Wong <yw340@georgetown.edu>
2017-12-15 19:26:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,5 +70,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+  if (cardNumber !== undefined){
+    console.log(`Your total cost is $${total()}, 
+    which will be charged to the card ${cardNumber}`);
+    cart = {};
+  }
 }
